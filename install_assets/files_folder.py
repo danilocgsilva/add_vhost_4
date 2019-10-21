9ce96657359b5c3f743fdeb8a200a71db6e6f4ca
Danilo Silva <danilo.silva@doc88.com.br>
2019-10-21 14:50:46 -0300

    Added new files to installation

--- install_assets/files_folder.py
+++ install_assets/files_folder.py
@@ -3,11 +3,13 @@ import os
 files = [
     '__main__.py',
     os.path.join('add_vhost_4', 'CLI.py'),
+    os.path.join('add_vhost_4', 'DebianLikeVHostEntry.py'),
     os.path.join('add_vhost_4', 'Hosts.py'),
     os.path.join('add_vhost_4', 'VHost_Entry.py'),
     os.path.join('add_vhost_4', 'vhost_config.template'),
     os.path.join('add_vhost_4', 'Guessers', 'Posix_Guesser.py'),
-    os.path.join('add_vhost_4', 'Guessers', 'Windows_Guesser.py')
+    os.path.join('add_vhost_4', 'Guessers', 'Windows_Guesser.py'),
+    os.path.join('add_vhost_4', 'Guessers', 'DebianLike_Guesser.py')
 ]
 
 folders = [
