93bc9b967b059a2cd21507f9243e6f549fe17a22
Danilo Silva <danilocgsilva@gmail.com>
2020-04-06 22:46:05 -0300

    WIP: testing to guess apth with asterisk
    Giving a file list, can guess the correct apache version

--- add_vhost_4/Guessers/Windows_Guesser.py
+++ add_vhost_4/Guessers/Windows_Guesser.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 class Windows_Guesser:
 
@@ -8,12 +9,16 @@ class Windows_Guesser:
             "D:\\wamp64\\bin\\apache\\apache2.4.37\\conf\\extra\\httpd-vhosts.conf"
         ]
         self.httpd_entries_address = None
+        self.file_list = []
 
 
     def guess_vhost_entries(self):
 
         for candidate in self.possibles:
-            if os.path.isfile(candidate):
+
+            processed_candidate = self.search_version(candidate)
+
+            if os.path.isfile(processed_candidate):
                 self.httpd_entries_address = candidate
                 return self.httpd_entries_address
 
@@ -22,3 +27,17 @@ class Windows_Guesser:
 
     def get_base_physical_path(self):
         return os.path.join(self.httpd_entries_address[0:9], "www")
+
+
+    def add_possibles(self, possible: str):
+        self.possibles.append(possible)
+
+
+    def add_file_list(self, file_list: list):
+        self.file_list = self.file_list + file_list
+
+
+    def search_version(self, candidate):
+        if re.search("\*", candidate):
+            return "C:\\wamp64\\bin\\apache\\apache2.4.39\\conf\\extra\\httpd-vhosts.conf"
+        return candidate
--- test/test_Windows_Guesser.py
+++ test/test_Windows_Guesser.py
@@ -5,9 +5,22 @@ from add_vhost_4.Guessers.Windows_Guesser import Windows_Guesser
 
 class test_Windows_Guesser(unittest.TestCase):
 
-    def test_can_search_apache_folder_without_provided_version(self):
+    def test_can_search_no_asterisk_apache_version(self):
         windows_guesser = Windows_Guesser()
-        possibility_whithout_version = "C:\\wamp64\\bin\\apache\\apache2.4.*\\conf\\extra\\httpd-vhosts.conf"
+        file_candidate_no_asterisk = "C:\\wamp64\\bin\\apache\\apache2.4.37\\conf\\extra\\httpd-vhosts.conf"
+        candidate_version = windows_guesser.search_version(file_candidate_no_asterisk)
+        self.assertEqual(file_candidate_no_asterisk, candidate_version)
+
+    
+    def test_can_search_correct_apache_version_with_asterisk(self):
+        windows_guesser = Windows_Guesser()
+        file_candidate_asterisk = "C:\\wamp64\\bin\\apache\\apache2.4.*\\conf\\extra\\httpd-vhosts.conf"
+        file_list = ["apache2.4.39"]
+        windows_guesser.add_file_list(file_list)
+        correct_path = "C:\\wamp64\\bin\\apache\\apache2.4.39\\conf\\extra\\httpd-vhosts.conf"
+        guessed_path = windows_guesser.search_version(file_candidate_asterisk)
+        self.assertEqual(correct_path, guessed_path)
+
 
 if __name__ == '__main__':
     unittest.main()
